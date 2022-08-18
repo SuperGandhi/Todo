1e954fca0f51be8a64f6e2c470a21c5130d7a383
SuperGandhi <patrickborjabueno@hotmail.com>
2022-08-18 16:21:50 -0500

    Incomplete

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
-const form  = document.getElementById('form');
-const input = document.getElementById('input');
-const todos = document.getElementById('todos');
+const form  = document.getElementById("form");
+const input = document.getElementById("input");
+const todos = document.getElementById("todos");
 
-form.addEventListener("submit", e =>{
+form.addEventListener("submit", (e) =>{
     e.preventDefault();
+    addTodo()
+});
 
+function addTodo(){
+        
     const todoText = input.value;
+
     if(todoText) {
-        const todoEl= document.createElement("li")
-        todoEl.innerText = todoText
-        todos.appendChild(todoText);
-
-        todoEl.addEventListener("click", () =>{
-            todoEl.classList.toggle("complete");
-        });
-
-        todos.appendChild(todoEl);
-        input.value = "";
-
-    }
-})
+            const todoEl= document.createElement("li")
+            todoEl.innerText = todoText;
+    
+    
+            todoEl.addEventListener("click", () =>{
+                todoEl.classList.toggle("complete");
+            });
+    
+            todoEl.addEventListener("contextmenu" ,(e) => {
+                e.preventDefault();
+    
+                todoEl.remove()
+            });
+    
+            todos.appendChild(todoEl);
+            input.value = "";
+        }
+}
