5d9ee3c0c49eccf0fc23c9d351bd6f3ad864bb9b
SuperGandhi <patrickborjabueno@hotmail.com>
2022-08-31 22:53:45 -0500

    Complete

--- index.js
+++ index.js
@@ -1,32 +1,69 @@
-const form  = document.getElementById("form");
+const form = document.getElementById("form");
 const input = document.getElementById("input");
-const todos = document.getElementById("todos");
+const todosUL = document.getElementById("todos");
 
-form.addEventListener("submit", (e) =>{
+const todos = JSON.parse(localStorage.getItem("todos"));
+
+if (todos) {
+    todos.forEach((todo) => {
+        addTodo(todo);
+    });
+}
+
+form.addEventListener("submit", (e) => {
     e.preventDefault();
-    addTodo()
+
+    addTodo();
 });
 
-function addTodo(){
-        
-    const todoText = input.value;
+function addTodo(todo) {
+    let todoText = input.value;
 
-    if(todoText) {
-            const todoEl= document.createElement("li")
-            todoEl.innerText = todoText;
-    
-    
-            todoEl.addEventListener("click", () =>{
-                todoEl.classList.toggle("complete");
-            });
-    
-            todoEl.addEventListener("contextmenu" ,(e) => {
-                e.preventDefault();
-    
-                todoEl.remove()
-            });
-    
-            todos.appendChild(todoEl);
-            input.value = "";
+    if (todo) {
+        todoText = todo.text;
+    }
+
+    if (todoText) {
+        const todoEl = document.createElement("li");
+        if (todo && todo.completed) {
+            todoEl.classList.add("completed");
         }
+
+        todoEl.innerText = todoText;
+
+        todoEl.addEventListener("click", () => {
+            todoEl.classList.toggle("completed");
+
+            updateLS();
+        });
+
+        todoEl.addEventListener("contextmenu", (e) => {
+            e.preventDefault();
+
+            todoEl.remove();
+
+            updateLS();
+        });
+
+        todosUL.appendChild(todoEl);
+
+        input.value = "";
+
+        updateLS();
+    }
+}
+
+function updateLS() {
+    const todosEl = document.querySelectorAll("li");
+
+    const todos = [];
+
+    todosEl.forEach((todoEl) => {
+        todos.push({
+            text: todoEl.innerText,
+            completed: todoEl.classList.contains("completed"),
+        });
+    });
+
+    localStorage.setItem("todos", JSON.stringify(todos));
 }
